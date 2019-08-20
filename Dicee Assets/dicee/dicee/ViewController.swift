61068913e691a36c59ee03f0d555f236660408d3
Shezan Mahmud <20822943+imshezan@users.noreply.github.com>
2019-08-20 15:42:17 +0600

    Add files via upload

---
+++ Dicee Assets/dicee/dicee/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Nazmus Sakib on 8/18/19.
+//  Copyright © 2019 ConstantMD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6",]
+
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImage()
+    }
+
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+            updateDiceImage()
+    }
+    
+    func updateDiceImage(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+}
+
